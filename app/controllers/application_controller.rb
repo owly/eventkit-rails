08c584f41a27b7c10315741549c893e7b6579d44
owly haham <lemuel@haham.net>
2017-01-17 00:30:34 +0000

    ...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+	around_action :log_everything, only: :index
+
 	# Prevent CSRF attacks by raising an exception.
 	# For APIs, you may want to use :null_session instead.
 	# protect_from_forgery with: :null_session
@@ -34,4 +36,27 @@ class ApplicationController < ActionController::Base
 		token = request.headers["X-Auth-Token"]
 		User.where(token: token).first
 	end
+	
+private
+def log_everything
+  log_headers
+  yield
+ensure
+  log_response
+end
+
+def log_headers
+  http_envs = {}.tap do |envs|
+    request.headers.each do |key, value|
+      envs[key] = value if key.downcase.starts_with?('http')
+    end
+  end
+
+  logger.info "Received #{request.method.inspect} to #{request.url.inspect} from #{request.remote_ip.inspect}. Processing with headers #{http_envs.inspect} and params #{params.inspect}"
+end
+
+def log_response
+  logger.info "Responding with #{response.status.inspect} => #{response.body.inspect}"
+end	
+	
 end
